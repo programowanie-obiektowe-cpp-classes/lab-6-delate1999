403d6baf22b94a866b63ad0f0619af44f52f1ad9
MK <whatever@nevermind.com>
2022-12-14 21:54:28 +0100

    Zaliczenie - V1.0

--- foo.hpp
+++ foo.hpp
@@ -5,12 +5,16 @@
 #include <algorithm>
 #include <list>
 #include <vector>
+#include <iostream>
 
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
 
     // Twoja implementacja tutaj
+    std::for_each(people.begin(), people.end(), [&] (Human& person) { person.birthday(); });
+    std::transform(people.cbegin(), people.cend(), retval.begin(), [](const Human& person) { return person.isMonster() ? 'n' : 'y'; });
+    std::reverse(retval.begin(), retval.end());
 
     return retval;
 }
